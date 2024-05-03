61325a5d8dde13e7123b068afacc6fe3e5fe54be
Nick Bruggeman <nick.bruggeman92@gmail.com>
2024-05-03 13:16:41 -0700

    changes to routes, models, css, js in public

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,16 @@
-// import sequelize
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-// establishing database connection with provided creds
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: 'localhost',
-    dialect: 'postgres'
-  }
-);
+const sequelize = process.env.DB_URL
+  ? new Sequelize(process.env.DB_URL)
+  : new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: 'localhost',
+      dialect: 'postgres',
+    }
+  );
 
 module.exports = sequelize;
