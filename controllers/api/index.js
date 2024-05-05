95db3cf42240b7a48af8b23674da21064c74ef3b
Nick Bruggeman <nick.bruggeman92@gmail.com>
2024-05-05 11:53:48 -0700

    index.js modified in controllers

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,13 @@
 const router = require('express').Router();
+
 const userRoutes = require('./userRoutes');
 
+const postRoutes= require('./postRoutes')
+
+const commentsRoutes=require('./commentsRoutes')
+
 router.use('/users', userRoutes);
+router.use('/post', postRoutes);
+router.use('/comments',commentsRoutes);
 
 module.exports = router;
